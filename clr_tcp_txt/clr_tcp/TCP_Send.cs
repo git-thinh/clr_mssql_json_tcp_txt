af5bd680400eb3ac4a8e0cec4d5581836cc8039f
nguyenvanthinh <nguyenvanthinh@f88.vn>
2020-02-24 10:19:39 +0700

    a

---
+++ clr_tcp_txt/clr_tcp/TCP_Send.cs
@@ -0,0 +1,373 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Net.Sockets;
+using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction()]
+    public static SqlString tcp___send_text(String host, Int32 port, String text)
+    {
+        if (string.IsNullOrEmpty(host) || port == 0 || string.IsNullOrEmpty(text))
+            return new SqlString("#ERROR: Para is null");
+
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(host, port);
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
+
+            stream.Flush();
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            return new SqlString(string.Format("#ERROR:{0}", ex.Message));
+        }
+
+        return new SqlString("#OK");
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static SqlString tcp___send_query(String host, Int32 port, String query)
+    {
+        if (string.IsNullOrEmpty(host) || port == 0 || string.IsNullOrEmpty(query))
+            return new SqlString("#ERROR: Para is null");
+
+        int k = 0;
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(host, port);
+            NetworkStream stream = client.GetStream();
+
+            using (SqlConnection conn = new SqlConnection("context connection=true"))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    var columns = new List<string>();
+                    for (var i = 0; i < reader.FieldCount; i++)
+                    {
+                        columns.Add(reader.GetName(i));
+                    }
+
+                    stream.Write(new byte[] { 91 }, 0, 1); // [
+
+                    while (reader.Read())
+                    {
+                        var dic = new Dictionary<string, object>() { { "ix___", k } };
+
+                        for (var i = 0; i < reader.FieldCount; i++)
+                            dic.Add(columns[i], reader.GetValue(i));
+
+                        if (k != 0) stream.Write(new byte[] { 44 }, 0, 1); // ,
+
+                        string json = JsonConvert.SerializeObject(dic, Formatting.Indented);
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                        stream.Write(buffer, 0, buffer.Length);
+
+                        k++;
+                    }
+
+                    stream.Write(new byte[] { 93 }, 0, 1); // ]
+
+                    //while (reader.Read())
+                    //{ 
+                    //    string json = subfix + JsonConvert.SerializeObject(reader.GetValue(0).ToString(), Formatting.Indented);
+                    //    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                    //    stream.Write(buffer, 0, buffer.Length); //sends bytes to server
+                    //}
+                }
+            }
+            stream.Flush();
+            stream.Close();
+            client.Close();
+
+        }
+        catch (Exception ex)
+        {
+            return new SqlString(string.Format("#ERROR:{0}", ex.Message));
+        }
+
+        return new SqlString("#OK:" + k.ToString());
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static SqlString tcp___send_cache(String host, Int32 port, String cache_name, String query)
+    {
+        if (string.IsNullOrEmpty(host) || port == 0 || string.IsNullOrEmpty(query) || string.IsNullOrEmpty(cache_name))
+            return new SqlString("#ERROR: Para is null");
+        if (cache_name.Length > 255)
+            return new SqlString("#ERROR: cache_name must be length < 256");
+
+        int k = 0;
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(host, port);
+            NetworkStream stream = client.GetStream();
+
+            using (SqlConnection conn = new SqlConnection("context connection=true"))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    var columns = new List<string>();
+                    for (var i = 0; i < reader.FieldCount; i++)
+                    {
+                        columns.Add(reader.GetName(i));
+                    }
+
+                    List<byte> ls = new List<byte>();
+                    ls.AddRange(System.Text.Encoding.ASCII.GetBytes(cache_name));
+                    int max = 257 - ls.Count;
+                    for (int i = 0; i < max; i++) ls.Add(32);
+                    stream.Write(ls.ToArray(), 0, ls.Count);
+
+                    stream.Write(new byte[] { 91 }, 0, 1); // [
+
+                    while (reader.Read())
+                    {
+                        var dic = new Dictionary<string, object>() { { "ix___", k } };
+
+                        for (var i = 0; i < reader.FieldCount; i++)
+                            dic.Add(columns[i], reader.GetValue(i));
+
+                        if (k != 0) stream.Write(new byte[] { 44 }, 0, 1); // ,
+
+                        string json = JsonConvert.SerializeObject(dic, Formatting.Indented);
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                        stream.Write(buffer, 0, buffer.Length);
+
+                        k++;
+                    }
+
+                    stream.Write(new byte[] { 93 }, 0, 1); // ]
+
+                    //while (reader.Read())
+                    //{ 
+                    //    string json = subfix + JsonConvert.SerializeObject(reader.GetValue(0).ToString(), Formatting.Indented);
+                    //    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                    //    stream.Write(buffer, 0, buffer.Length); //sends bytes to server
+                    //}
+                }
+            }
+            stream.Flush();
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            return new SqlString(string.Format("#ERROR:{0}", ex.Message));
+        }
+
+        return new SqlString("#OK:" + k.ToString());
+    }
+
+    static string[] arr1 = new string[] { "á", "à", "?", "ã", "?", "â", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?",
+    "?",
+    "é","è","?","?","?","ê","?","?","?","?","?",
+    "í","ì","?","?","?",
+    "ó","ò","?","õ","?","ô","?","?","?","?","?","?","?","?","?","?","?",
+    "ú","ù","?","?","?","?","?","?","?","?","?",
+    "ý","?","?","?","?"};
+    static string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
+    "d",
+    "e","e","e","e","e","e","e","e","e","e","e",
+    "i","i","i","i","i",
+    "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",
+    "u","u","u","u","u","u","u","u","u","u","u",
+    "y","y","y","y","y"};
+
+    static string convert_Unicode_2_Ascii(string str)
+    {
+        if (string.IsNullOrWhiteSpace(str)) return str;
+
+        for (int i = 0; i < arr1.Length; i++)
+            str = str.Replace(arr1[i], arr2[i]);
+
+        str = str.Replace("e?", "e").Replace("o?", "o").Replace("a?", "o");
+
+        return str;
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static SqlString tcp___send_cache_indexs(String host, Int32 port, String cache_name, String query,
+        String col_id, String col_ascii, String col_utf8, String col_org,
+        String col_1n)
+    {
+        if (string.IsNullOrEmpty(host) || port == 0 || string.IsNullOrEmpty(query) || string.IsNullOrEmpty(cache_name))
+            return new SqlString("#ERROR: Para is null");
+        if (cache_name.Length > 255)
+            return new SqlString("#ERROR: cache_name must be length < 256");
+
+        int k = 0;
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(host, port);
+            NetworkStream stream = client.GetStream();
+
+            bool has_all_ids = col_id == "*" ? true : false,
+                 has_all_ascci = col_ascii == "*" ? true : false,
+                 has_all_utf8 = col_utf8 == "*" ? true : false,
+                 has_all_org = col_org == "*" ? true : false;
+
+            string[] a_ids = string.IsNullOrEmpty(col_id) && col_id.Length > 0 ? new string[] { } : col_id.ToLower().Split(',').Select(x => x.Trim()).ToArray();
+            string[] a_ascii = string.IsNullOrEmpty(col_ascii) && col_ascii.Length > 0 ? new string[] { } : col_ascii.ToLower().Split(',').Select(x => x.Trim()).ToArray();
+            string[] a_utf8 = string.IsNullOrEmpty(col_utf8) && col_utf8.Length > 0 ? new string[] { } : col_utf8.ToLower().Split(',').Select(x => x.Trim()).ToArray();
+            string[] a_org = string.IsNullOrEmpty(col_org) && col_org.Length > 0 ? new string[] { } : col_org.ToLower().Split(',').Select(x => x.Trim()).ToArray();
+            string[] a_1n = string.IsNullOrEmpty(col_1n) && col_1n.Length > 0 ? new string[] { } : col_1n.ToLower().Split(',').Select(x => x.Trim()).ToArray();
+            bool existID = false;
+            long id = 0;
+            bool exist1n = false;
+
+            using (SqlConnection conn = new SqlConnection("context connection=true"))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    var columns = new string[reader.FieldCount];
+                    var types = new string[reader.FieldCount];
+                    var dtypes = new Dictionary<string, string>() { };
+                    var cell = new Dictionary<string, int>() { };
+                    for (var i = 0; i < reader.FieldCount; i++)
+                    {
+                        columns[i] = reader.GetName(i).ToLower();
+                        types[i] = reader.GetFieldType(i).Name.ToLower();
+                        dtypes.Add(columns[i], types[i]);
+                        cell.Add(columns[i], i);
+                        if (columns[i] == "id" && (types[i] == "int32" || types[i] == "int64")) existID = true;
+                        if (col_1n == columns[i]) exist1n = true;
+                    }
+
+                    List<byte> ls = new List<byte>();
+                    ls.AddRange(Encoding.UTF8.GetBytes(cache_name));
+                    int max = 257 - ls.Count;
+                    for (int i = 0; i < max; i++) ls.Add(32);
+                    stream.Write(ls.ToArray(), 0, ls.Count);
+
+                    stream.Write(new byte[] { 123 }, 0, 1); // {
+
+                    var bf = Encoding.UTF8.GetBytes(@"""items"":");
+                    stream.Write(bf, 0, bf.Length);
+
+                    var dIndex = new Dictionary<long, int>();
+                    var d1n = new Dictionary<long, List<long>>();
+
+                    var col_numbers = dtypes.Where(x => x.Key != "id" && (x.Value == "byte" || x.Value == "int32" || x.Value == "int64")).Select(x => x.Key).ToArray();
+                    var col_text = dtypes.Where(x => x.Value == "string").Select(x => x.Key).ToArray();
+
+                    stream.Write(new byte[] { 91 }, 0, 1); // [
+                    while (reader.Read())
+                    {
+                        var dic = new Dictionary<string, object>() { { "ix___", k } };
+                        for (var i = 0; i < reader.FieldCount; i++)
+                            dic.Add(columns[i], reader.GetValue(i));
+
+                        var _numbers = new List<string>();
+
+                        if (existID)
+                        {
+                            switch (dtypes["id"])
+                            {
+                                case "int32":
+                                    id = (Int32)dic["id"];
+                                    break;
+                                case "int64":
+                                    id = (Int64)dic["id"];
+                                    break;
+                            }
+                            dIndex.Add(id, k);
+                            _numbers.Add(id.ToString());
+                        }
+
+                        if (existID && exist1n)
+                        {
+                            if (dic[col_1n] != null)
+                            {
+                                long _1n = -1;
+                                switch (dtypes[col_1n])
+                                {
+                                    case "int32":
+                                        _1n = (Int32)dic[col_1n];
+                                        break;
+                                    case "int64":
+                                        _1n = (Int64)dic[col_1n];
+                                        break;
+                                }
+
+                                if (_1n > -1)
+                                {
+                                    if (d1n.ContainsKey(_1n) == false) d1n.Add(_1n, new List<long>() { id });
+                                    else d1n[_1n].Add(id);
+                                }
+                            }
+                        }
+
+
+                        //IEnumerable<string> _text = new string[] { };
+                        var av_text = col_text.Select(x => dic[x]).Where(x => x != null).Select(x => x.ToString());
+                        _numbers.AddRange(col_numbers.Select(x => dic[x]).Where(x => x != null).Select(x => x.ToString()).Where(x => x != "-1"));
+
+                        string nums = string.Join(" ", _numbers);
+                        string _text = string.Join(" ", av_text).ToLower().Trim();
+                        _text = Regex.Replace(_text, @"\s+", " ");
+                        string _ascii = convert_Unicode_2_Ascii(_text);
+
+                        dic.Add("#ids", nums);
+                        dic.Add("#ascii", _ascii);
+                        dic.Add("#utf8", _text);
+
+                        if (k != 0) stream.Write(new byte[] { 44 }, 0, 1); // ,
+                        string json = JsonConvert.SerializeObject(dic);
+                        byte[] buffer = Encoding.UTF8.GetBytes(json);
+                        stream.Write(buffer, 0, buffer.Length);
+
+                        k++;
+                    }
+
+                    stream.Write(new byte[] { 93 }, 0, 1); // ]
+
+                    bf = System.Text.Encoding.UTF8.GetBytes(@",""columns"":" + JsonConvert.SerializeObject(dtypes)
+                        + @",""indexs"":" + JsonConvert.SerializeObject(dIndex)
+                        + @",""index1n"":" + JsonConvert.SerializeObject(d1n));
+                    stream.Write(bf, 0, bf.Length);
+
+                    stream.Write(new byte[] { 125 }, 0, 1); // }
+
+                    //while (reader.Read())
+                    //{ 
+                    //    string json = subfix + JsonConvert.SerializeObject(reader.GetValue(0).ToString(), Formatting.Indented);
+                    //    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                    //    stream.Write(buffer, 0, buffer.Length); //sends bytes to server
+                    //}
+                }
+            }
+            stream.Flush();
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            return new SqlString(string.Format("#ERROR:{0}", ex.Message));
+        }
+
+        return new SqlString("#OK:" + k.ToString());
+    }
+
+}
---
+++ clr_tcp_txt/clr_tcp_txt/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace clr_tcp_txt
+{
+    class Program
+    {
+        //const int CHUNK_SIZE = 65535;
+        const int CHUNK_SIZE = 1 * 1024 * 1024; //1MB
+        const int _1MB = 1024 * 1024; //1MB
+
+        static string title = "";
+
+        static void Main(string[] args)
+        {
+            string _ip = "192.168.11.205";
+            int _port = 6969;
+
+            if (!Directory.Exists("txt")) Directory.CreateDirectory("txt");
+
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse(_ip);
+                TcpListener listener = new TcpListener(ipAddress, _port);
+
+                title = string.Format("TCP {0}:{1}", _ip, _port);
+                Console.Title = title;
+
+                listener.Start();
+                while (true)
+                {
+                    Socket client = listener.AcceptSocket();
+
+                    var childSocketThread = new Thread(new ParameterizedThreadStart((o) =>
+                    {
+                        int total = 0;
+                        int k = 0;
+                        int len = 0;
+                        byte[] buffer = new byte[CHUNK_SIZE];
+                        var socket = (Socket)o;
+
+                        string file_name = "";
+
+                        using (var stream = new MemoryStream())
+                        {
+                            byte[] actual;
+
+                            Stopwatch stopwatch = new Stopwatch();
+                            stopwatch.Start();
+
+                            while ((len = socket.Receive(buffer, buffer.Length, SocketFlags.None)) > 0)
+                            {
+                                total += len;
+
+                                if (k == 0)
+                                {
+                                    if (len < 255)
+                                    {
+                                        Console.WriteLine("Buffer invalid ...");
+                                        break;
+                                    }
+                                    file_name = Encoding.ASCII.GetString(buffer, 0, 255).Trim().ToUpper();
+
+                                    Console.WriteLine("-> " + file_name + " buffering ...");
+
+                                    actual = new byte[len - 255];
+                                    Buffer.BlockCopy(buffer, 255, actual, 0, len - 255);
+                                }
+                                else
+                                {
+                                    actual = new byte[len];
+                                    Buffer.BlockCopy(buffer, 0, actual, 0, len);
+                                }
+
+                                if (k % 5 == 0) Console.Title = String.Format("{0:N0} MB", total / _1MB);
+
+                                stream.Write(actual, 0, actual.Length);
+                                k++;
+                            }
+
+
+                            if (file_name.Length > 0)
+                            {
+                                string file = "txt\\" + file_name + ".txt";
+                                if (File.Exists(file)) File.Delete(file);
+
+                                //var fileStream = File.Create(file);
+                                //stream.Seek(0, SeekOrigin.Begin);
+                                //stream.CopyTo(fileStream);
+                                //fileStream.Close();
+
+                                int fileSize = (int)stream.Length;
+                                using (FileStream fileStream = new FileStream(file, FileMode.Create, System.IO.FileAccess.Write))
+                                {
+                                    stream.Seek(0, SeekOrigin.Begin);
+                                    byte[] bytes = new byte[fileSize];
+                                    stream.Read(bytes, 0, fileSize);
+                                    fileStream.Write(bytes, 0, fileSize);
+                                    stream.Close();
+                                }
+                            }
+
+                            stopwatch.Stop();
+                            TimeSpan ts = stopwatch.Elapsed;
+                            string elapsedTime = String.Format(" | Duration: {0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                            string s = string.Format("-> {0} OK = {1}", file_name, String.Format("{0:N0} MB", total / _1MB)) + elapsedTime;
+                            Console.Title = string.Format("{0} -> {1}", title, s);
+                            Console.WriteLine(s);
+                        }
+
+                        socket.Close();
+                    }
+                    ));
+                    childSocketThread.Start(client);
+                }
+
+                listener.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.StackTrace);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
